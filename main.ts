cd23ebbd6a5de5c1e77907a535add8a3ae337d1c
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-05-17 17:00:38 -0300

    Criando o servidor com o restify e a primeira rota

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log('Teste 12344');
+import restify from 'restify';
+
+// Server create
+const server = restify.createServer({
+    name: "meat-api",
+    version: "1.0.0"
+});
+
+// Register a route
+server.get('/hello', (req, res, next) => {
+    res.json({message: 'hello'});
+    return next();
+});
+
+// Server listen port
+server.listen(3000, () => {
+    console.log('API is running on http://localhost:3000');
+});
