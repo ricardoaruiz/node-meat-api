e4ec7e65b0e99166a5582f89d23258ff514b3a44
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-05-21 11:57:45 -0300

    Criando validação personalizada de CPF no mongoose

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { validateCPF } from '../common/validators'
 
 export interface UserDocument extends mongoose.Document {
     name: string,
@@ -29,6 +30,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: false,
         enum: ['Male','Female']
+    },
+    cpf: {
+        type: String,
+        required: false,
+        validate: {
+            validator: validateCPF,
+            message: '{PATH} Invalid CPF ({VALUE})'
+        }
     }
 });
 
